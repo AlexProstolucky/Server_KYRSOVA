e0b4bd6342c8cd4a925433fbb348ad83a4534e5c
4elovel <marcynakvova007@gmail.com>
2024-02-13 23:47:26 +0200

    CRD

--- ConsoleApp1/DataAccess/Utils/ApplicationContext.cs
+++ ConsoleApp1/DataAccess/Utils/ApplicationContext.cs
@@ -29,4 +29,116 @@ public class ApplicationContext : DbContext
         .HasIndex(u => u.Email)
         .IsUnique();
     }
+    public async Task<User> GeUserByIdAsync(Guid id)
+    {
+
+        return await Users.Where(u => u.Id == id).Include(u => u.Friends).FirstOrDefaultAsync();
+    }
+    public User GeUserById(Guid id)
+    {
+
+        return Users.Where(u => u.Id == id).Include(u => u.Friends).FirstOrDefault();
+    }
+    public async Task<User> GeUserByEmailAsync(string email)
+    {
+
+        return await Users.Where(u => u.Email == email).Include(u => u.Friends).FirstOrDefaultAsync();
+    }
+    public User GeUserByIEmail(string email)
+    {
+
+        return Users.Where(u => u.Email == email).Include(u => u.Friends).FirstOrDefault();
+    }
+    public async Task<User> GeUserByLoginAsync(string login)
+    {
+
+        return await Users.Where(u => u.Login == login).Include(u => u.Friends).FirstOrDefaultAsync();
+    }
+    public User GeUserByLogin(string login)
+    {
+
+        return Users.Where(u => u.Login == login).Include(u => u.Friends).FirstOrDefault();
+    }
+    public bool IsLogged(User user)
+    {
+        return Users.Where(u => u.Equals(user)).Any();
+    }
+    public async Task<bool> IsLoggedAsync(User user)
+    {
+        return await Users.Where(u => u.Equals(user)).AnyAsync();
+    }
+    public bool IsLogged(string login, string password)
+    {
+        return Users.Where(u => u.Login.Equals(login) && u.Password.Equals(password)).Any();
+    }
+    public async Task<bool> IsLoggedAsync(string login, string password)
+    {
+        return await Users.Where(u => u.Login.Equals(login) && u.Password.Equals(password)).AnyAsync();
+    }
+    public bool AddUser(User user)
+    {
+        if (user == null) return false;
+        Users.Add(user);
+        this.SaveChanges();
+        return true;
+    }
+    public async Task<bool> AddUserAsync(User user)
+    {
+        if (user == null) return false;
+        await Users.AddAsync(user);
+        await SaveChangesAsync();
+        return true;
+    }
+    public bool RemoveUser(User user)
+    {
+        if (user == null) return false;
+        var realUser = Users.Where(u => u.Id == user.Id).FirstOrDefault();
+        if (realUser == null) return false;
+        Users.Remove(realUser);
+        this.SaveChanges();
+        return true;
+    }
+    public async Task<bool> RemoveUserAsync(User user)
+    {
+        if (user == null) return false;
+        var realUser = await Users.Where(u => u.Id == user.Id).FirstOrDefaultAsync();
+        if (realUser == null) return false;
+        Users.Remove(realUser);
+        await SaveChangesAsync();
+        return true;
+    }
+    public bool RemoveUser(Guid id)
+    {
+        var realUser = Users.Where(u => u.Id == id).FirstOrDefault();
+        if (realUser == null) return false;
+        Users.Remove(realUser);
+        this.SaveChanges();
+        return true;
+    }
+    public async Task<bool> RemoveUserAsync(Guid id)
+    {
+        var realUser = await Users.Where(u => u.Id == id).FirstOrDefaultAsync();
+        if (realUser == null) return false;
+        Users.Remove(realUser);
+        await SaveChangesAsync();
+        return true;
+    }
+    public bool RemoveUserByLogin(string login)
+    {
+        if (login == null) return false;
+        var realUser = Users.Where(u => u.Login == login).FirstOrDefault();
+        if (realUser == null) return false;
+        Users.Remove(realUser);
+        this.SaveChanges();
+        return true;
+    }
+    public async Task<bool> RemoveUserByLoginAsync(string login)
+    {
+        if (login == null) return false;
+        var realUser = await Users.Where(u => u.Login == login).FirstOrDefaultAsync();
+        if (realUser == null) return false;
+        Users.Remove(realUser);
+        await SaveChangesAsync();
+        return true;
+    }
 }
